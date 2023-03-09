bb80ce1abf803eb3947d5cd1e3a678980a6a9cbf
ryan pujo <pujotriantoro@gmail.com>
2023-03-09 23:14:39 +0700

    adding go route to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:ryan_pujo_app/init.dart';
 import 'package:ryan_pujo_app/user/application/bloc/user_bloc_bloc.dart';
 import 'package:ryan_pujo_app/user/infrastructure/repository/user_repo_contract.dart';
-import 'package:ryan_pujo_app/user/presentation/pages/homepage.dart';
+
+import 'user/presentation/pages/splash_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,21 +13,28 @@ void main() {
   runApp(const MyApp());
 }
 
+final _route = GoRouter(routes: [
+  GoRoute(
+    path: "/",
+    builder: (context, state) => const SplashPage(),
+  )
+]);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: BlocProvider(
-        create: (context) => UserBlocBloc(locator<UserRepositoryContract>()),
-        child: Homepage(),
+    return BlocProvider(
+      create: (context) => UserBlocBloc(locator<UserRepositoryContract>()),
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        routerConfig: _route,
       ),
     );
   }
